4c3c25db115c2f1376083fbf59cdf642fc3cae2e
Franco Solleza <iam@francosolleza.com>
2021-12-02 12:20:34 +0000

    chunk push tests

--- src/chunk/mod.rs
+++ src/chunk/mod.rs
@@ -134,3 +134,118 @@ impl FlushChunk {
     }
 }
 
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::test_utils::*;
+    use crate::segment::{self, Segment};
+
+    #[test]
+    fn test_push_behavior() {
+        let data = &MULTIVARIATE_DATA[0].1;
+        let nvars = data[0].values.len();
+        let segment = Segment::new(3, nvars);
+        let mut writer = segment.writer().unwrap();
+        let mut flusher = segment.flusher().unwrap();
+
+        let mut chunk = Chunk::new(0, Compression::LZ4(1));
+        let mut chunk_writer = chunk.writer().unwrap();
+
+        let mut to_values = |items: &[f64]| -> Vec<[u8; 8]> {
+            let mut values = vec![[0u8; 8]; nvars];
+            for (i, v) in items.iter().enumerate() {
+                values[i] = v.to_be_bytes();
+            }
+            values
+        };
+
+        for item in &data[..256 * CHUNK_THRESHOLD_COUNT - 1] {
+            let v = to_values(&item.values[..]);
+            match writer.push(item.ts, &v[..]) {
+                Ok(segment::PushStatus::Done) => {},
+                Ok(segment::PushStatus::Flush) => {
+                    let seg = flusher.to_flush().unwrap();
+                    assert_eq!(chunk_writer.push(&seg), Ok(PushStatus::Done));
+                    flusher.flushed();
+                },
+                Err(_) => unimplemented!(),
+            }
+        }
+
+        {
+            let item = &data[256 * CHUNK_THRESHOLD_COUNT];
+            let v = to_values(&item.values[..]);
+            assert_eq!(writer.push(item.ts, &v[..]), Ok(segment::PushStatus::Flush));
+            let seg = flusher.to_flush().unwrap();
+            assert_eq!(chunk_writer.push(&seg), Ok(PushStatus::Flush));
+            flusher.flushed();
+        }
+
+        for item in &data[256 * CHUNK_THRESHOLD_COUNT..256 * (CHUNK_THRESHOLD_COUNT + 1)] {
+            let v = to_values(&item.values[..]);
+            match writer.push(item.ts, &v[..]) {
+                Ok(segment::PushStatus::Done) => {},
+                Ok(segment::PushStatus::Flush) => { break; },
+                Err(_) => unimplemented!(),
+            }
+        }
+
+        {
+            let seg = flusher.to_flush().unwrap();
+            assert_eq!(chunk_writer.push(&seg), Err(Error::PushIntoFull));
+        }
+    }
+
+    //#[test]
+    //fn test_push_flush_data() {
+    //    let data = &MULTIVARIATE_DATA[0].1;
+    //    let nvars = data[0].values.len();
+    //    let segment = Segment::new(3, nvars);
+    //    let mut writer = segment.writer().unwrap();
+    //    let mut flusher = segment.flusher().unwrap();
+
+    //    let mut to_values = |items: &[f64]| -> Vec<[u8; 8]> {
+    //        let mut values = vec![[0u8; 8]; nvars];
+    //        for (i, v) in items.iter().enumerate() {
+    //            values[i] = v.to_be_bytes();
+    //        }
+    //        values
+    //    };
+
+    //    let mut exp_ts = Vec::new();
+    //    let mut exp_values = Vec::new();
+    //    for _ in 0..nvars {
+    //        exp_values.push(Vec::new());
+    //    }
+
+    //    for item in &data[..767] {
+    //        let v = to_values(&item.values[..]);
+    //        assert!(writer.push(item.ts, &v[..]).is_ok());
+    //        exp_ts.push(item.ts);
+    //        for (e, i) in exp_values.iter_mut().zip(v.iter()) {
+    //            e.push(*i)
+    //        }
+    //    }
+
+    //    let seg = flusher.to_flush().unwrap();
+    //    assert_eq!(seg.len, 256);
+    //    assert_eq!(seg.nvars, nvars);
+    //    assert_eq!(seg.timestamps(), &exp_ts[..256]);
+    //    for i in 0..nvars {
+    //        assert_eq!(seg.values(i), &exp_values[i][..256]);
+    //    }
+    //    flusher.flushed();
+
+    //    let seg = flusher.to_flush().unwrap();
+    //    assert_eq!(seg.len, 256);
+    //    assert_eq!(seg.timestamps(), &exp_ts[256..512]);
+    //    for i in 0..nvars {
+    //        assert_eq!(seg.values(i), &exp_values[i][256..512]);
+    //    }
+    //    flusher.flushed();
+
+    //    assert!(flusher.to_flush().is_none()) // the current buffer is not flushable yet
+    //}
+
+}
+
