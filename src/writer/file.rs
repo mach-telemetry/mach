3b20e4cec2bfc2456023227a485840b300a336d1
Franco Solleza <iam@francosolleza.com>
2021-12-12 15:56:11 +0000

    move filewriter to file.rs

--- src/writer/file.rs
+++ src/writer/file.rs
@@ -1,25 +1,14 @@
-use crate::{backend::fs, chunk, segment, tags::Tags};
+use crate::{SeriesMetadata, backend::fs, chunk, segment, tags::Tags, writer::{PushStatus, Error}};
 use async_std::channel::{unbounded, Receiver, Sender};
 //use crossbeam::queue::SegQueue;
-use std::sync::{
-    atomic::{AtomicU64, Ordering::SeqCst},
-    Arc,
-};
-
-#[derive(Debug)]
-pub enum Error {
-    Segment(segment::Error),
-}
-
-impl From<segment::Error> for Error {
-    fn from(item: segment::Error) -> Self {
-        Error::Segment(item)
+use dashmap::DashMap;
+use std::{
+    collections::{hash_map::Entry, HashMap},
+    sync::{
+        atomic::{AtomicU64, Ordering::SeqCst},
+        Arc,
     }
-}
-
-pub enum PushStatus {
-    Done,
-}
+};
 
 pub struct ToFlush {
     segment: segment::FlushSegment,
@@ -112,3 +101,80 @@ async fn file_flush_worker(file_allocator: Arc<AtomicU64>, queue: Receiver<ToFlu
         item.flush(&mut file);
     }
 }
+
+struct Metadata {
+    writer: SeriesWriter,
+    meta: Arc<SeriesMetadata>,
+}
+
+pub struct FileWriter {
+    reference: Arc<DashMap<Tags, Arc<SeriesMetadata>>>, // communicate with Global
+    id_map: HashMap<Tags, u64>,
+    thread_id: u64,
+    flush_worker: Arc<FlushWorker>,
+    writers: Vec<Metadata>,
+}
+
+impl FileWriter {
+    fn new(
+        thread_id: Option<u64>,
+        reference: Arc<DashMap<Tags, Arc<SeriesMetadata>>>,
+        shared_file: Arc<AtomicU64>,
+    ) -> Self {
+        let flush_worker = Arc::new(FlushWorker::new(shared_file));
+        let thread_id = match thread_id {
+            Some(x) => x,
+            None => std::thread::current().id().as_u64().get(),
+        };
+        FileWriter {
+            reference,
+            flush_worker,
+            id_map: HashMap::new(),
+            thread_id,
+            writers: Vec::new(),
+        }
+    }
+
+    fn init_series(&mut self, tags: Tags) -> Result<u64, Error> {
+        let entry = self.id_map.entry(tags.clone());
+        match entry {
+            Entry::Occupied(_) => Err(Error::SeriesReinitialized),
+            Entry::Vacant(x) => match self.reference.get(&tags) {
+                Some(item) => {
+                    if item.thread_id.load(SeqCst) == self.thread_id {
+                        let id = self.writers.len();
+                        let segment = item.segment.clone();
+                        let chunk = item.chunk.clone();
+                        let list = item.list.clone();
+                        let flush_worker = self.flush_worker.clone();
+                        let writer = SeriesWriter::new(
+                            tags.clone(),
+                            segment,
+                            chunk,
+                            list,
+                            flush_worker,
+                        );
+                        let meta = Metadata {
+                            writer,
+                            meta: item.clone(),
+                        };
+                        self.writers.push(meta);
+                        Ok(id as u64)
+                    } else {
+                        Err(Error::SeriesNotFound)
+                    }
+                }
+                None => Err(Error::SeriesNotFound),
+            },
+        }
+    }
+
+    fn get_reference_id(&mut self, tags: &Tags) -> Option<u64> {
+        Some(*(self.id_map.get(&tags)?))
+    }
+
+    fn push(&mut self, id: u64, ts: u64, values: &[[u8; 8]]) {
+        self.writers[id as usize].writer.push(ts, values).unwrap();
+    }
+}
+
