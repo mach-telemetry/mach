a275ba740820fbf9666e80a6a71aac3efd9ed1e0
Franco Solleza <iam@francosolleza.com>
2022-04-28 15:57:34 -0400

    adding a print for the args

--- bin/src/tsdb.rs
+++ bin/src/tsdb.rs
@@ -22,10 +22,13 @@ use none_tsdb::NoneTSDB;
 
 #[derive(Parser, Debug)]
 struct Args {
-    #[clap(short, long, default_value_t = String::from("Mach"))]
+    #[clap(short, long, default_value_t = String::from("mach"))]
     tsdb: String,
 
-    #[clap(short, long, default_value_t = String::from("127.0.0.1:50050"))]
+    #[clap(short, long, default_value_t = String::from("127.0.0.1"))]
+    addr: String,
+
+    #[clap(short, long, default_value_t = String::from("50050"))]
     port: String,
 }
 
@@ -53,17 +56,22 @@ pub fn increment_sample_counter(v: usize) {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
-    let addr = args.port.as_str().parse()?;
+    println!("Args: {:?}", args);
+    let mut addr = String::from(args.addr.as_str());
+    addr.push(':');
+    addr.push_str(args.port.as_str());
+    let addr = addr.parse()?;
+    let _counter = COUNTER.load(SeqCst); // init counter
 
     match args.tsdb.as_str() {
-        "Mach" => {
+        "mach" => {
             let tsdb = MachTSDB::new();
             Server::builder()
                 .add_service(TsdbServiceServer::new(tsdb))
                 .serve(addr)
                 .await?;
         },
-        "None" => {
+        "none" => {
             let tsdb = NoneTSDB::new();
             Server::builder()
                 .add_service(TsdbServiceServer::new(tsdb))
