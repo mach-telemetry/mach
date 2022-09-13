a11d5b89ca39e5f7a34b4ab82857ae55accf068e
Franco Solleza <iam@francosolleza.com>
2022-09-13 15:59:16 -0400

    printing stats for sources, will modify

--- bin/src/completeness-workload.rs
+++ bin/src/completeness-workload.rs
@@ -27,6 +27,8 @@ use std::fs::File;
 use std::io::*;
 
 use kafka_utils::KafkaTopicOptions;
+use rand::prelude::*;
+use rand_chacha::ChaCha8Rng;
 
 lazy_static! {
     static ref ARGS: Args = Args::parse();
@@ -41,12 +43,18 @@ lazy_static! {
     };
     static ref SAMPLES: Vec<(SeriesId, &'static [SampleType])> = {
         let keys: Vec<SeriesId> = BASE_DATA.keys().copied().collect();
-
         println!("Expanding data based on source_count = {}", ARGS.source_count);
-        let mut rng = rand::thread_rng();
+        let mut rng = ChaCha8Rng::seed_from_u64(1);;
         let mut tmp_samples = Vec::new();
+        let mut stats_map: Vec<(bool, usize)> = Vec::new();
         for id in 0..ARGS.source_count {
             let s = BASE_DATA.get(keys.choose(&mut rng).unwrap()).unwrap();
+            // count metrics
+            let is_metric = match s[0].1[0] {
+                SampleType::F64(_) => true,
+                _ => false,
+            };
+            stats_map.push((is_metric, s.len()));
             for item in s.iter() {
                 tmp_samples.push((SeriesId(id), item.0, item.1.as_slice()));
             }
@@ -57,7 +65,20 @@ lazy_static! {
 
         println!("Setting up final samples");
         let samples: Vec<(SeriesId, &[SampleType])> = tmp_samples.drain(..).map(|x| (x.0, x.2)).collect();
-        println!("Samples len: {}", samples.len());
+
+        println!("Samples stats:");
+        println!("Total number of unique samples: {}", samples.len());
+        let metrics_count: u64 = stats_map.iter().map(|x| x.0 as u64).sum();
+        let average_source_length: u64 = {
+            let sum: u64 = stats_map.iter().map(|x| x.1 as u64).sum();
+            sum / (stats_map.len() as u64)
+        };
+        let max_source_length: u64 = stats_map.iter().map(|x| x.1 as u64).max().unwrap();
+        let min_source_length: u64 = stats_map.iter().map(|x| x.1 as u64).min().unwrap();
+        println!("Number of sources: {}, Number of metrics: {}", stats_map.len(), metrics_count);
+        println!("Max source length: {}", max_source_length);
+        println!("Average source length: {}", average_source_length);
+        println!("Min source length: {}", min_source_length);
         samples
     };
     static ref SERIES_IDS: Vec<SeriesId> = {
