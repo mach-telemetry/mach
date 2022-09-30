a664f9cac89b80c69bb500a1f1427ab5aaf874e0
vic-shihang-li <40274755+vic-shihang-li@users.noreply.github.com>
2022-09-30 18:38:15 -0400

    use the latest kafka setup

--- bin/src/kafka_bench.rs
+++ bin/src/kafka_bench.rs
@@ -1,14 +1,10 @@
+mod kafka_utils;
+
 use clap::*;
+use kafka_utils::{make_topic, Producer};
 use lazy_static::lazy_static;
 use num_format::{Locale, ToFormattedString};
 use rand::{thread_rng, Rng};
-use rdkafka::{
-    admin::{AdminClient, AdminOptions, NewTopic, TopicReplication},
-    client::DefaultClientContext,
-    config::ClientConfig,
-    producer::FutureProducer,
-    producer::FutureRecord,
-};
 use std::{
     sync::atomic::{AtomicU64, Ordering::SeqCst},
     thread,
@@ -23,15 +19,15 @@ macro_rules! mb_to_bytes {
 
 lazy_static! {
     static ref TOPIC: String = uuid::Uuid::new_v4().to_string();
-    static ref KAFKA_PARTITIONS: i32 = 3;
-    static ref KAFKA_REPLICATION: i32 = 3;
-    static ref COUNTER: AtomicU64 = AtomicU64::new(0);
+    static ref BYTES_WRITTEN: AtomicU64 = AtomicU64::new(0);
 }
 
 #[derive(Parser, Debug, Clone)]
 struct Args {
-    #[clap(short, long, default_value_t = 1)]
-    writers: usize,
+    #[clap(short, long, default_value_t = 3)]
+    writer_partitions: i32,
+    #[clap(short, long, default_value_t = 3)]
+    replicas: i32,
     #[clap(short, long, default_value_t = String::from("localhost:9093,localhost:9094,localhost:9095"))]
     bootstrap_servers: String,
     /// Number of bytes in a batch written to kafka.
@@ -46,7 +42,7 @@ fn collect_median(threshold: usize) -> u64 {
     let mut prev = 0;
     let mut collected = Vec::new();
     loop {
-        let curr = COUNTER.load(SeqCst);
+        let curr = BYTES_WRITTEN.load(SeqCst);
 
         let bytes_sent = curr - prev;
         prev = curr;
@@ -65,42 +61,11 @@ fn collect_median(threshold: usize) -> u64 {
     }
 }
 
-fn default_producer(bootstraps: String) -> FutureProducer {
-    println!("making producer to bootstraps: {}", bootstraps);
-    let producer: FutureProducer = ClientConfig::new()
-        .set("bootstrap.servers", bootstraps)
-        .set("message.max.bytes", "1000000000")
-        .set("message.copy.max.bytes", "5000000")
-        .set("compression.type", "none")
-        .set("acks", "all")
-        .set("message.timeout.ms", "10000")
-        .create()
-        .unwrap();
-    producer
-}
-
-fn make_topic(bootstrap_servers: &str) {
-    let topic = TOPIC.clone();
-    let client: AdminClient<DefaultClientContext> = ClientConfig::new()
-        .set("bootstrap.servers", bootstrap_servers.to_string())
-        .create()
-        .unwrap();
-    let admin_opts = AdminOptions::new().request_timeout(Some(Duration::from_secs(5)));
-    let topics = &[NewTopic {
-        name: topic.as_str(),
-        num_partitions: *KAFKA_PARTITIONS,
-        replication: TopicReplication::Fixed(*KAFKA_REPLICATION),
-        config: Vec::new(),
-    }];
-    futures::executor::block_on(client.create_topics(topics, &admin_opts)).unwrap();
-    println!("topic created: {}", TOPIC.as_str());
-}
-
 fn make_payload(num_bytes: usize) -> Vec<u8> {
     (0..num_bytes).map(|_| thread_rng().gen()).collect()
 }
 
-fn kafka_write(producer: FutureProducer, payload: &[u8]) {
+fn kafka_write(mut producer: Producer, partition: i32, payload: &[u8]) {
     let sync_interval = Duration::from_secs(1);
     let mut last_synced = Instant::now();
     let mut bytes_sent = 0;
@@ -108,11 +73,10 @@ fn kafka_write(producer: FutureProducer, payload: &[u8]) {
     let payload_len: u64 = payload.len().try_into().unwrap();
 
     loop {
-        let to_send: FutureRecord<str, [u8]> = FutureRecord::to(&TOPIC).payload(payload);
-        futures::executor::block_on(producer.send(to_send, Duration::from_secs(0))).unwrap();
+        producer.send(&TOPIC, partition, payload);
         bytes_sent += payload_len;
         if last_synced.elapsed() > sync_interval {
-            COUNTER.fetch_add(bytes_sent, SeqCst);
+            BYTES_WRITTEN.fetch_add(bytes_sent, SeqCst);
             last_synced = Instant::now();
             bytes_sent = 0;
         }
@@ -123,13 +87,18 @@ fn main() {
     let args = Args::parse();
     println!("Args:\n{:#?}", args);
 
-    make_topic(args.bootstrap_servers.as_str());
+    let opts = kafka_utils::KafkaTopicOptions {
+        num_partitions: args.writer_partitions,
+        num_replicas: args.replicas,
+    };
+    make_topic(args.bootstrap_servers.as_str(), TOPIC.as_str(), opts);
+
     let payload = make_payload(args.batch_size);
 
-    for _ in 0..args.writers {
-        let producer = default_producer(args.bootstrap_servers.clone());
+    for partition in 0..args.writer_partitions {
+        let producer = Producer::new(args.bootstrap_servers.as_str());
         let my_payload = payload.clone();
-        thread::spawn(move || kafka_write(producer, my_payload.as_slice()));
+        thread::spawn(move || kafka_write(producer, partition, my_payload.as_slice()));
     }
 
     let median = collect_median(args.total_bytes);
