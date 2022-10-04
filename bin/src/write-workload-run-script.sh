22176d956e879fed435b955f7d4c3aa8e654c4b1
Ubuntu <ubuntu@ip-172-31-22-116.ec2.internal>
2022-10-04 01:04:00 +0000

    rerun 1 writer

--- bin/src/write-workload-run-script.sh
+++ bin/src/write-workload-run-script.sh
@@ -13,19 +13,19 @@ KAFKA_BATCH_BYTES=1000000
 KAFKA_BOOTSTRAPS=localhost:9093,localhost:9094,localhost:9095
 KAFKA_BOOTSTRAPS=b-2.machkafka.ypemck.c25.kafka.us-east-1.amazonaws.com:9092,b-1.machkafka.ypemck.c25.kafka.us-east-1.amazonaws.com:9092,b-3.machkafka.ypemck.c25.kafka.us-east-1.amazonaws.com:9092
 
-#cargo run --release --bin kafka-write-workload -- \
-#	--kafka-bootstraps $KAFKA_BOOTSTRAPS \
-#	--file-path $FILE_PATH \
-#	--writer-batches $WRITER_BATCHES \
-#	--data-generator-count $DATA_GENERATORS \
-#	--kafka-writers $WRITER_COUNT \
-#	--kafka-partitions $KAFKA_PARTITIONS \
-#	--kafka-batch-bytes $KAFKA_BATCH_BYTES
-
-cargo run --release --bin mach-write-workload -- \
+cargo run --release --bin kafka-write-workload -- \
+	--kafka-bootstraps $KAFKA_BOOTSTRAPS \
 	--file-path $FILE_PATH \
 	--writer-batches $WRITER_BATCHES \
 	--data-generator-count $DATA_GENERATORS \
-	--mach-writers $WRITER_COUNT \
+	--kafka-writers $WRITER_COUNT \
+	--kafka-partitions $KAFKA_PARTITIONS \
+	--kafka-batch-bytes $KAFKA_BATCH_BYTES
+
+#cargo run --release --bin mach-write-workload -- \
+#	--file-path $FILE_PATH \
+#	--writer-batches $WRITER_BATCHES \
+#	--data-generator-count $DATA_GENERATORS \
+#	--mach-writers $WRITER_COUNT \
 
 
