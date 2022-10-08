2c473d68e62577b5e0a60605da01a183835455d6
Ubuntu <ubuntu@ip-172-31-22-116.ec2.internal>
2022-10-08 13:26:40 +0000

    parse simple query results

---
+++ bin/src/evaluation_output/query_execution_time.py
@@ -0,0 +1,21 @@
+FILE = "mach_simple_query_20221008124909"
+
+def parse_line(line):
+    parts = line.split(",")
+    current_time = parts[0].split(" ")[2]
+    query_id = int(parts[1].split(":")[1])
+    data_latency = float(parts[3].split(":")[1])
+    execution_latency = float(parts[3].split(":")[1])
+    return current_time, query_id, data_latency, execution_latency
+
+
+with open(FILE, "r") as f:
+    lines = f.readlines()
+    lines = list(filter(lambda x: x.startswith("Current"), lines))
+
+print("current_time, query_id, data_latency, execution_latency");
+for line in lines:
+    current_time, query_id, data_latency, execution_latency = parse_line(line)
+    print("{}, {}, {:.10f}, {:.10f}".format(current_time, query_id, data_latency, execution_latency))
+
+
