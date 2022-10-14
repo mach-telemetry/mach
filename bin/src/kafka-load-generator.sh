fde69205b9a0a7dfff7d63f527272c953545e8ed
vic-shihang-li <40274755+vic-shihang-li@users.noreply.github.com>
2022-10-14 03:01:45 +0000

    fix kafka load generator load to 300mbps

--- bin/src/kafka-load-generator.sh
+++ bin/src/kafka-load-generator.sh
@@ -1,6 +1,6 @@
 WRITER_COUNT=150
 BOOTSTRAPS=b-3.k1.ge0foy.c17.kafka.us-east-1.amazonaws.com:9092,b-1.k1.ge0foy.c17.kafka.us-east-1.amazonaws.com:9092,b-2.k1.ge0foy.c17.kafka.us-east-1.amazonaws.com:9092
-RATE=1000
+RATE=300
 PARTITIONS=24
 
 cargo run --release --bin kafka-load-generator --\
