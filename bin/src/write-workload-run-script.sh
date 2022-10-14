0502465c2b3c32c11d3669633b924865acfd3f18
Ubuntu <ubuntu@ip-172-31-22-116.ec2.internal>
2022-10-14 15:24:15 +0000

    write workload for CPU bound reads

--- bin/src/write-workload-run-script.sh
+++ bin/src/write-workload-run-script.sh
@@ -16,35 +16,35 @@ UNBOUNDED_QUEUE=--unbounded-queue
 KAFKA_PARTITIONS=4
 KAFKA_BATCH_BYTES=1000000
 KAFKA_BOOTSTRAPS=localhost:9093,localhost:9094,localhost:9095
-KAFKA_BOOTSTRAPS=b-3.k2.aaozun.c17.kafka.us-east-1.amazonaws.com:9092,b-2.k2.aaozun.c17.kafka.us-east-1.amazonaws.com:9092,b-1.k2.aaozun.c17.kafka.us-east-1.amazonaws.com:9092
+KAFKA_BOOTSTRAPS=b-2.k1.aacwd3.c17.kafka.us-east-1.amazonaws.com:9092,b-1.k1.aacwd3.c17.kafka.us-east-1.amazonaws.com:9092,b-3.k1.aacwd3.c17.kafka.us-east-1.amazonaws.com:9092
 
 QUERIER_IP=172.31.78.194
 
 KAFKA_OUT_FILE=${OUTPUT}/kafka_ingest_${WRITER_COUNT}_writers_${WRITER_BATCHES}_batch_${SOURCE_COUNT}_sources_$(date +"%Y%m%d%H%M%S")
-KAFKA_OUT_FILE=${OUTPUT}/kafka_tmp
+#KAFKA_OUT_FILE=${OUTPUT}/kafka_tmp
 
-#cargo run --release --bin kafka-write-workload -- \
-#	--kafka-bootstraps $KAFKA_BOOTSTRAPS \
-#	--file-path $FILE_PATH \
-#	--writer-batches $WRITER_BATCHES \
-#	--data-generator-count $DATA_GENERATORS \
-#	--kafka-writers $WRITER_COUNT \
-#	--kafka-partitions $KAFKA_PARTITIONS \
-#	--kafka-batch-bytes $KAFKA_BATCH_BYTES \
-#	--querier-ip $QUERIER_IP \
-#	${UNBOUNDED_QUEUE} \
-#	> $KAFKA_OUT_FILE
-
-
-MACH_OUT_FILE=${OUTPUT}/mach_ingest_${WRITER_COUNT}_writers_${WRITER_BATCHES}_batch_${SOURCE_COUNT}_sources_$(date +"%Y%m%d%H%M%S")
-MACH_OUT_FILE=${OUTPUT}/mach_tmp
-
-cargo run --release --bin mach-write-workload -- \
+cargo run --release --bin kafka-write-workload -- \
+	--kafka-bootstraps $KAFKA_BOOTSTRAPS \
 	--file-path $FILE_PATH \
 	--writer-batches $WRITER_BATCHES \
 	--data-generator-count $DATA_GENERATORS \
-	--mach-writers $WRITER_COUNT \
-	--source-count $SOURCE_COUNT \
+	--kafka-writers $WRITER_COUNT \
+	--kafka-partitions $KAFKA_PARTITIONS \
+	--kafka-batch-bytes $KAFKA_BATCH_BYTES \
 	--querier-ip $QUERIER_IP \
 	${UNBOUNDED_QUEUE} \
-	> $MACH_OUT_FILE
+	> $KAFKA_OUT_FILE
+
+
+MACH_OUT_FILE=${OUTPUT}/mach_ingest_${WRITER_COUNT}_writers_${WRITER_BATCHES}_batch_${SOURCE_COUNT}_sources_$(date +"%Y%m%d%H%M%S")
+#MACH_OUT_FILE=${OUTPUT}/mach_tmp
+
+#cargo run --release --bin mach-write-workload -- \
+#	--file-path $FILE_PATH \
+#	--writer-batches $WRITER_BATCHES \
+#	--data-generator-count $DATA_GENERATORS \
+#	--mach-writers $WRITER_COUNT \
+#	--source-count $SOURCE_COUNT \
+#	--querier-ip $QUERIER_IP \
+#	${UNBOUNDED_QUEUE} \
+#	> $MACH_OUT_FILE
