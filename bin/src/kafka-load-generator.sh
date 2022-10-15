907252f692f0cb86f967878381db5111d9d94e1c
Ubuntu <ubuntu@ip-172-31-30-127.ec2.internal>
2022-10-15 13:13:56 +0000

    some changes to the way we're loading kafka

--- bin/src/kafka-load-generator.sh
+++ bin/src/kafka-load-generator.sh
@@ -1,12 +1,13 @@
-WRITER_COUNT=64
-BOOTSTRAPS=b-2.francocluster.fxyjf4.c17.kafka.us-east-1.amazonaws.com:9092,b-3.francocluster.fxyjf4.c17.kafka.us-east-1.amazonaws.com:9092,b-1.francocluster.fxyjf4.c17.kafka.us-east-1.amazonaws.com:9092
-RATE=300
+WRITER_COUNT=128
+BOOTSTRAPS=b-1.francocluster2.wseolp.c17.kafka.us-east-1.amazonaws.com:9092,b-2.francocluster2.wseolp.c17.kafka.us-east-1.amazonaws.com:9092,b-3.francocluster2.wseolp.c17.kafka.us-east-1.amazonaws.com:9092
+RATE=1000
 PARTITIONS=24
-TOPICS=4
+TOPICS=16
 
 cargo run --release --bin kafka-load-generator --\
 	--writer-count $WRITER_COUNT \
 	--bootstrap-servers $BOOTSTRAPS \
 	--rate $RATE \
-	--kafka-partitions $PARTITIONS
+	--kafka-partitions $PARTITIONS \
+	--kafka-topics $TOPICS
 
