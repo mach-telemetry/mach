051548628d0506c6b2180d436f0bb6ddbf2740b6
Ubuntu <ubuntu@ip-172-31-78-194.ec2.internal>
2022-10-15 07:17:27 +0000

    repeat result on 1mblock, 1 metablock writer

--- bin/src/query-workload-run-script.sh
+++ bin/src/query-workload-run-script.sh
@@ -2,7 +2,7 @@
 
 ES_INDEX=es06
 
-FILE_PATH=/home/ubuntu/processed-data.bin
+FILE_PATH=/home/ubuntu/data/processed-data.bin
 OUTPUT=evaluation_output
 QUERY_MIN_DELAY=3
 QUERY_MAX_DELAY=10
@@ -12,17 +12,17 @@ KAFKA_BOOTSTRAPS=b-3.k2.ini5pv.c17.kafka.us-east-1.amazonaws.com:9092,b-1.k2.ini
 
 ES_ENDPOINT=https://vpc-es01-6wvpspb2kiqdbokovy7svaxqli.us-east-1.es.amazonaws.com
 
-SNAPSHOT_SERVER_PORT=https://172.31.31.117:50051
+SNAPSHOT_SERVER_PORT=https://172.31.22.116:50051
 
-#cargo run --release --bin simple-mach-query -- \
-#	--query-count 100 \
-#	--file-path $FILE_PATH \
-#	--snapshot-server-port $SNAPSHOT_SERVER_PORT \
-#	--query-min-delay $QUERY_MIN_DELAY \
-#	--query-max-delay $QUERY_MAX_DELAY \
-#	--query-min-duration $QUERY_MIN_DURATION \
-#	--query-max-duration $QUERY_MAX_DURATION \
-#	> mach_query_out
+cargo run --release --bin simple-mach-query -- \
+	--query-count 150 \
+	--file-path $FILE_PATH \
+	--snapshot-server-port $SNAPSHOT_SERVER_PORT \
+	--query-min-delay $QUERY_MIN_DELAY \
+	--query-max-delay $QUERY_MAX_DELAY \
+	--query-min-duration $QUERY_MIN_DURATION \
+	--query-max-duration $QUERY_MAX_DURATION \
+	> ${OUTPUT}/mach_simple_query_$(date +"%Y%m%d%H%M%S")
 
 #cargo run --release --bin simple-kafka-query -- \
 #	--query-count 100 \
@@ -35,13 +35,13 @@ SNAPSHOT_SERVER_PORT=https://172.31.31.117:50051
 #	> ${OUTPUT}/kafka_query_out
 #	#> ${OUTPUT}/kafka_simple_query_$(date +"%Y%m%d%H%M%S")
 
-cargo run --release --bin simple-es-query -- \
-    --query-count 1000 \
-	--query-min-delay $QUERY_MIN_DELAY \
-	--query-max-delay $QUERY_MAX_DELAY \
-	--query-min-duration $QUERY_MIN_DURATION \
-	--query-max-duration $QUERY_MAX_DURATION \
-    --file-path $FILE_PATH \
-    --es-endpoint $ES_ENDPOINT \
-    --es-index-name $ES_INDEX \
-    > ${OUTPUT}/es_query_out
+#cargo run --release --bin simple-es-query -- \
+#    --query-count 1000 \
+#	--query-min-delay $QUERY_MIN_DELAY \
+#	--query-max-delay $QUERY_MAX_DELAY \
+#	--query-min-duration $QUERY_MIN_DURATION \
+#	--query-max-duration $QUERY_MAX_DURATION \
+#    --file-path $FILE_PATH \
+#    --es-endpoint $ES_ENDPOINT \
+#    --es-index-name $ES_INDEX \
+#    > ${OUTPUT}/es_query_out
