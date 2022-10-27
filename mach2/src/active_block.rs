f2ec5db70784d21640694b29b82dc4a92e8d34e2
Franco Solleza <iam@francosolleza.com>
2022-10-27 11:57:13 -0400

    starting with pub struct of ActiveBlock

--- mach2/src/active_block.rs
+++ mach2/src/active_block.rs
@@ -2,7 +2,7 @@ use crate::{
     active_segment::ActiveSegmentRef, byte_buffer::ByteBuffer, compression::Compression,
     constants::*, id::SourceId, mem_list::data_block::DataBlock,
 };
-use std::sync::atomic::{AtomicUsize, Ordering::SeqCst};
+use std::sync::{Arc, atomic::{AtomicUsize, Ordering::SeqCst}};
 use serde::*;
 use std::cell::UnsafeCell;
 
@@ -19,14 +19,111 @@ pub struct BlockMetadata {
     max_ts: u64,
 }
 
+impl BlockMetadata {
+    fn new() -> Self {
+        Self {
+            source_id: SourceId(u64::MAX),
+            offset: usize::MAX,
+            min_ts: u64::MAX,
+            max_ts: u64::MAX,
+        }
+    }
+}
+
+
 pub struct ReadOnlyBlock {
     data: Vec<u8>,
     offsets: Vec<BlockMetadata>
 }
 
+pub struct ActiveBlockWriter {
+    inner: Arc<InnerActiveBlock>
+}
+
+impl ActiveBlockWriter {
+    //pub fn push(
+    //    &mut self,
+    //    source_id: SourceId,
+    //    active_segment: ActiveSegmentRef,
+    //    compression: &Compression
+    //) -> {
+    //}
+}
+
+pub struct ActiveBlock {
+    inner: Arc<InnerActiveBlock>
+}
+
+impl ActiveBlock {
+    pub fn new() -> (ActiveBlock, ActiveBlockWriter) {
+        let boxed = box InnerActiveBlock {
+            inner: UnsafeCell::new(Inner {
+                data: [0u8; BLOCK_SZ * 2],
+                data_len: AtomicUsize::new(0),
+                offsets: [BlockMetadata::new(); BLOCK_SZ],
+                offsets_len: AtomicUsize::new(0),
+            }),
+            version: AtomicUsize::new(0)
+        };
+        let inner: Arc<InnerActiveBlock> = boxed.into();
+        let active_block = Self {
+            inner: inner.clone()
+        };
+        let writer = ActiveBlockWriter {
+            inner,
+        };
+        (active_block, writer)
+    }
+
+    pub fn read_only(&self) -> Result<ReadOnlyBlock, &'static str> {
+        self.inner.read_only()
+    }
+}
+
 struct InnerActiveBlock {
     version: AtomicUsize,
     inner: UnsafeCell<Inner>
+}
+
+impl InnerActiveBlock {
+    fn push(&self,
+        source_id: SourceId,
+        active_segment: ActiveSegmentRef,
+        compression: &Compression
+    ) -> PushStatus {
+
+        // Safety This function does not race with any other function
+        unsafe {
+            (*self.inner.get()).push(source_id, active_segment, compression)
+        }
+    }
+
+    fn reset(&self) -> DataBlock {
+        self.version.fetch_add(1, SeqCst);
+        let ptr = self.inner.get();
+        // Safety: Close does not race with other functions. Reset does (with read_only). Version
+        // makes sure read_only will return an error if they race.
+        let data_block = unsafe {
+            let data_block = (*ptr).close();
+            (*ptr).reset();
+            data_block
+        };
+        self.version.fetch_add(1, SeqCst);
+        data_block
+    }
+
+    fn read_only(&self) -> Result<ReadOnlyBlock, &'static str> {
+        let version = self.version.load(SeqCst);
+        // Safety: versioning ensures that if read_only races with reset, will return error
+        let result = unsafe {
+            (*self.inner.get()).read_only()
+        };
+        if version != self.version.load(SeqCst) {
+            Err("Failed to snapshot active block")
+        } else {
+            Ok(result)
+        }
+    }
 }
 
 struct Inner {
@@ -73,7 +170,6 @@ impl Inner {
             .copy_from_slice(&chunk_sz.to_be_bytes());
 
         let new_len = byte_buffer.len();
-        self.data_len.store(new_len, SeqCst);
 
         let offsets_len = self.offsets_len.load(SeqCst);
         let metadata = BlockMetadata {
@@ -83,6 +179,7 @@ impl Inner {
             max_ts
         };
         self.offsets[offsets_len] = metadata;
+        self.data_len.store(new_len, SeqCst);
         self.offsets_len.fetch_add(1, SeqCst);
 
         if new_len > BLOCK_SZ || offsets_len + 1 == BLOCK_SZ {
