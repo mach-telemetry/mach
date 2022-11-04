e77744ebf054ce6759222a3638ef8bf2332e474f
Franco Solleza <iam@francosolleza.com>
2022-11-04 06:28:33 -0400

    starting tsdb

--- mach2/src/tsdb.rs
+++ mach2/src/tsdb.rs
@@ -1,36 +1,26 @@
-//use crate::source::*;
-//use crate::writer::{WriterId, WriterMetadata};
-//use crate::mem_list::metadata_list::MetadataList;
-//use crate::active_segment::ActiveSegment;
-//use rand::prelude::{thread_rng, SliceRandom};
-//use std::sync::Arc;
-//use dashmap::DashMap;
+use crate::snapshot::Snapshot;
+use crate::source::*;
+use crate::writer::Writer;
+use dashmap::DashMap;
+use std::sync::Arc;
 
-//pub struct Mach {
-//    sources: Arc<DashMap<SourceId, Source>>,
-//    to_writers: Arc<DashMap<SourceId, ActiveSegmentWriter>>,
-//    writer_table: Arc<DashMap<WriterId, WriterMetadata>>,
-//    writers: Vec<WriterId>,
-//}
-//
-//impl Mach {
-//    pub fn add_source(&self, config: SourceConfig) -> WriterId {
-//        let source_id = config.id;
-//        let (metadata_list, metadata_list_writer) = MetadataList::new();
-//        let (active_segment, active_segment_writer) = ActiveSegment::new(config.types.as_slice());
-//        //assert!(self.source_writers.insert(config.id, (active_segment_writer, metadata_list_writer)).is_none());
-//
-//        let writer_id = *self.writers.choose(&mut thread_rng()).unwrap();
-//        let active_block = self.writer_table.get(&writer_id).unwrap().active_block.clone();
-//        active_block.add_source(source_id, metadata_list_writer);
-//
-//        let source = Source {
-//            config,
-//            active_segment,
-//            metadata_list,
-//            active_block,
-//        };
-//        self.sources.insert(source_id, source);
-//        writer_id
-//    }
-//}
+pub struct Mach {
+    source_table: Arc<DashMap<SourceId, Source>>,
+}
+
+impl Mach {
+    pub fn new() -> Self {
+        Self {
+            source_table: Arc::new(DashMap::new()),
+        }
+    }
+
+    pub fn add_writer(&self) -> Writer {
+        Writer::new(self.source_table.clone())
+    }
+
+    pub fn snapshot(&self, id: SourceId) -> Snapshot {
+        let source = self.source_table.get(&id).unwrap().clone();
+        source.snapshot()
+    }
+}
